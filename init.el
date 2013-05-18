c164601c3b731aa157c58fc6f93310a65cf399f6
Robert Krahn <robert.krahn@gmail.com>
2013-05-17 17:19:02 -0700

    my new emacs config, now with org-mode init file, initial commit

---
+++ init.el
@@ -0,0 +1,11 @@
+(require 'org-install)
+(require 'ob-tangle)
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+;; tangle and load the init code
+(let* ((init-org-file (concat dotfiles-dir "init.org"))
+      (base-name (file-name-sans-extension init-org-file))
+      (exported-file (concat base-name "-tangled.el")))
+  (org-babel-tangle-file init-org-file exported-file "emacs-lisp")
+  (load-file exported-file)
+  (message "Loaded %s" exported-file))
+
