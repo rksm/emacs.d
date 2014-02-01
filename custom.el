d2fadb8c6fc8ceba977dde779d4942bc9e2550ce
Robert Krahn <robert.krahn@gmail.com>
2014-01-31 16:29:46 -0800

    custom.el update

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(Man-notify-method (quote pushy))
+ '(custom-safe-themes (quote ("1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" default)))
  '(erc-hide-list (quote ("JOIN" "PART" "QUIT")))
  '(erc-log-channels-directory "~/.erc/log")
  '(erc-log-insert-log-on-open t)
@@ -13,7 +14,7 @@
  '(erc-notifications-mode t t)
  '(erc-save-buffer-on-part t)
  '(erc-save-queries-on-quit t)
-  '(safe-local-variable-values (quote ((Base . 10) (Syntax . ANSI-Common-Lisp) (Package . CYC) (outline-minor-mode) (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark)))))
+ '(safe-local-variable-values (quote ((Base . 10) (Syntax . ANSI-Common-Lisp) (Package . CYC) (haskell-indent-offset 2) (outline-minor-mode) (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark) (js3-indent-level . 2)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
